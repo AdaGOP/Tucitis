bd77c922e3a120dbfb3bf4ffd0b58a30782179a3
David <david_gunawan_@hotmail.com>
2023-09-05 16:06:09 +0700

    Left out commit

--- Tucitis/TucitisActivityAttributes.swift
+++ Tucitis/TucitisActivityAttributes.swift
@@ -1,8 +1,20 @@
 //
-//  File.swift
+//  TucitisActivityAttributes.swift.swift
 //  Tucitis
 //
 //  Created by David Gunawan on 05/09/23.
 //
 
-import Foundation
+import SwiftUI
+import ActivityKit
+
+struct TucitisActivityAttributes: ActivityAttributes {
+    public typealias LiveCleaningStatus = ContentState
+    
+    public struct ContentState: Codable, Hashable {
+        var robotName: String
+        var cleaningTime: ClosedRange<Date>
+    }
+    
+    var coverageArea: Int
+}
--- WidgetExtension/TucitisLiveActivity.swift
+++ WidgetExtension/TucitisLiveActivity.swift
@@ -1,6 +1,6 @@
 //
-//  WidgetExtensionLiveActivity.swift
-//  WidgetExtension
+//  TucitisLiveActivity.swift
+//  TucitisLiveActivity
 //
 //  Created by David Gunawan on 05/09/23.
 //
@@ -9,46 +9,130 @@ import ActivityKit
 import WidgetKit
 import SwiftUI
 
-struct WidgetExtensionLiveActivity: Widget {
+struct TucitisLiveActivity: Widget {
     var body: some WidgetConfiguration {
-        ActivityConfiguration(for: WidgetExtensionAttributes.self) { context in
-            // Lock screen/banner UI goes here
-            VStack {
-                Text("Hello")
-            }
-            .activityBackgroundTint(Color.cyan)
-            .activitySystemActionForegroundColor(Color.black)
-
+        ActivityConfiguration(for: TucitisActivityAttributes.self) { context in
+            // MARK: - For devices that don't support the Dynamic Island.
+            VStack(alignment: .leading) {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("Your \(context.state.robotName) is on the way!")
+                            .font(.headline)
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 15)
+                                .fill(.secondary)
+                            HStack {
+                                RoundedRectangle(cornerRadius: 15)
+                                    .fill(.blue)
+                                    .frame(width: 50)
+                                Image(systemName: "shippingbox.circle.fill")
+                                    .foregroundColor(.white)
+                                    .padding(.leading, -25)
+                                Image(systemName: "arrow.forward")
+                                    .foregroundColor(.white.opacity(0.5))
+                                Image(systemName: "ellipsis")
+                                    .foregroundColor(.white.opacity(0.5))
+                                Text(timerInterval: context.state.cleaningTime, countsDown: true)
+                                    .bold()
+                                    .font(.caption)
+                                    .foregroundColor(.white.opacity(0.8))
+                                    .multilineTextAlignment(.center)
+                                Image(systemName: "ellipsis")
+                                    .foregroundColor(.white.opacity(0.5))
+                                Image(systemName: "arrow.forward")
+                                    .foregroundColor(.white.opacity(0.5))
+                                Image(systemName: "house.circle.fill")
+                                    .foregroundColor(.green)
+                                    .background(.white)
+                                    .clipShape(Circle())
+                            }
+                        }
+                    }
+                    Spacer()
+                    VStack {
+                        Text("2 🍕")
+                            .font(.title)
+                            .bold()
+                        Spacer()
+                    }
+                }.padding(5)
+                Text("You've already paid: $9.9 Delivery Fee 💸")
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+                    .padding(.horizontal, 5)
+            }.padding(15)
+            // MARK: - For Dynamic Island
         } dynamicIsland: { context in
             DynamicIsland {
-                // Expanded UI goes here.  Compose the expanded UI through
-                // various regions, like leading/trailing/center/bottom
                 DynamicIslandExpandedRegion(.leading) {
-                    Text("Leading")
+                    Label("2 Pizza", systemImage: "bag")
+                        .font(.title3)
                 }
                 DynamicIslandExpandedRegion(.trailing) {
-                    Text("Trailing")
+                    Label {
+                        Text(timerInterval: context.state.cleaningTime, countsDown: true)
+                            .multilineTextAlignment(.trailing)
+                            .frame(width: 50)
+                            .monospacedDigit()
+                            .font(.caption2)
+                    } icon: {
+                        Image(systemName: "timer")
+                    }
+                    .font(.title2)
+                }
+                DynamicIslandExpandedRegion(.center) {
+                    Text("\(context.state.robotName) is on the way!")
+                        .lineLimit(1)
+                        .font(.caption)
+                        .foregroundColor(.secondary)
                 }
                 DynamicIslandExpandedRegion(.bottom) {
-                    Text("Bottom")
-                    // more content
+                    // Deep Linking
+                    HStack {
+                        Link(destination: URL(string: "pizza://contact+TIM")!) {
+                             Label("Contact driver", systemImage: "phone.circle.fill")
+                                .font(.caption)
+                                .padding()
+                         }.background(Color.accentColor)
+                         .clipShape(RoundedRectangle(cornerRadius: 15))
+                        Spacer()
+                        Link(destination: URL(string: "pizza://cancelOrder")!) {
+                             Label("Cancel Order", systemImage: "xmark.circle.fill")
+                                .font(.caption)
+                                .padding()
+                         }.background(Color.red)
+                         .clipShape(RoundedRectangle(cornerRadius: 15))
+                    }
                 }
             } compactLeading: {
-                Text("L")
+                Label {
+                    Text("2 Pizza")
+                } icon: {
+                    Image(systemName: "bag")
+                }
+                .font(.caption2)
             } compactTrailing: {
-                Text("T")
+                Text(timerInterval: context.state.cleaningTime, countsDown: true)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 40)
+                    .font(.caption2)
             } minimal: {
-                Text("Min")
+                VStack(alignment: .center) {
+                    Image(systemName: "timer")
+                    Text(timerInterval: context.state.cleaningTime, countsDown: true)
+                        .multilineTextAlignment(.center)
+                        .monospacedDigit()
+                        .font(.caption2)
+                }
             }
-            .widgetURL(URL(string: "http://www.apple.com"))
-            .keylineTint(Color.red)
+            .keylineTint(.accentColor)
         }
     }
 }
 
-struct WidgetExtensionLiveActivity_Previews: PreviewProvider {
-    static let attributes = WidgetExtensionAttributes(name: "Me")
-    static let contentState = WidgetExtensionAttributes.ContentState(value: 3)
+struct TucitisLiveActivity_Previews: PreviewProvider {
+    static let attributes = TucitisActivityAttributes(coverageArea: 10)
+    static let contentState = TucitisActivityAttributes.ContentState(robotName: "Tucitis", cleaningTime: Date()...Date().addingTimeInterval(15 * 60))
 
     static var previews: some View {
         attributes
