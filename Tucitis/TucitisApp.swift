ab7ba04cbccdcca6a644d34e0e6bf7f63ecef5dd
Allicia Viona <alliciawn@gmail.com>
2023-09-05 10:34:10 +0700

    Initial Commit

---
+++ Tucitis/TucitisApp.swift
@@ -0,0 +1,17 @@
+//
+//  TucitisApp.swift
+//  Tucitis
+//
+//  Created by Allicia Viona Sagi on 05/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TucitisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
